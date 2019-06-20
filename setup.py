9e8f43877e71bfef250c0cb9f4ac5f98280248e0
PauTorru <pautorruellabesa@gmail.com>
2019-06-20 14:14:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ import time
 
 setup(
     name='PauPy',
-    packages=['PauPy'],
     description='Collection of TEM and EELS analysis functions',
     version='0.1',
     url='https://github.com/PauTorru/PauPy',
